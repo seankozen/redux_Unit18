be5ca98fea7864429b55a44586313270a1507284
Sean Kozen <sgkozen@gmail.com>
2022-09-13 22:03:57 +0900

    Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,28 @@
 import {createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {counter: 0, showCounter: true};
+
+//From Redux Toolkit
+createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter += action.amount;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        },
+    },
+
+});
 
 const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
